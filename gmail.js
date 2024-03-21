e137478c31a6fd03684c3b30c9a5cb7c8ef85b5d
SanthoshV437 <124693062+SanthoshV437@users.noreply.github.com>
2024-03-21 19:30:48 +0530

    welcome

--- gmail.js
+++ gmail.js
@@ -4,4 +4,4 @@ document.getElementById('gmail-btn').addEventListener('click', function () {
     var body = "Hello,\n\nThis is a test email."; // you can set the email body text here
     var gmail_link = "mailto:" + gmail_address + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
     window.location.href = gmail_link;
-});
+  });
